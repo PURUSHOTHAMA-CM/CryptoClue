7dad1076494e4c4a68e66a9ad7f55ee65fa18287
PURUSHOTHAMA-CM <purushothamacm1011@gmail.com>
2025-04-30 16:33:27 +0530

    Crypto clue complete

--- index.js
+++ index.js
@@ -3,11 +3,12 @@
 import express from "express";
 import axios from "axios";
 import bodyParser from "body-parser";
+import env from "dotenv";
 
-
+env.config();
 
 const app=express();
-const port=3000;
+const port=process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({extended:true}));
 
